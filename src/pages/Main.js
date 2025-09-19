9e99bd22be641069c665f88aa951af792eab6c2e
Hosein Rahimi <hosein81rahimi@gmail.com>
2025-09-19 12:37:06 +0330

    Update something

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,20 @@
+import PageTransition from "../components/common/PageTransition";
 import HomeCard1 from '../components/Main/HomeCard-1.jsx'
 import HomeCategories2 from '../components/Main/HomeCategories-2.jsx'
 import HomeCarousel3 from '../components/Main/HomeCarousel-3.jsx'
 
 export default function Main() {
-    return(
-        <div>
-            <br/>
-            <HomeCard1 />
-            <br/>
-            <HomeCategories2 />
-            <br/>
-            <HomeCarousel3 />
-        </div>
+    return (
+        <PageTransition>
+            <div>
+                <br />
+                <HomeCard1 />
+                <br />
+                <HomeCategories2 />
+                <br />
+                <HomeCarousel3 />
+            </div>
+            <h1>34567</h1>
+        </PageTransition>
     )
 }
