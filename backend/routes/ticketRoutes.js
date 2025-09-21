cf7e1d716af48fe7d03edf70c39c3e60be4033bd
Hosein Rahimi <hosein81rahimi@gmail.com>
2025-09-21 14:17:28 +0330

    new

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,30 +1,8 @@
-
 import express from "express";
 import { protect, support } from "../middleware/authMiddleware.js";
 import { createTicket, getAllTickets, answerTicket, getUserTickets, getSupportTickets, getTicketDetails, closeTicket, deleteTicket, setTicketReadForSupport, setTicketReadForUser, clearUserTicketNotification, reopenTicketByAdmin } from "../controllers/ticketController.js";
 
 
-// ایجاد تیکت توسط کاربر
-router.post("/", protect, createTicket);
-// دریافت لیست تیکت‌های یوزر
-router.get("/my", protect, getUserTickets);
-// دریافت لیست تیکت‌های ساپورت
-router.get("/all", protect, support, getSupportTickets);
-// دریافت جزئیات یک تیکت
-router.get("/:id", protect, getTicketDetails);
-// بستن تیکت
-router.put("/:id/close", protect, closeTicket);
-// حذف تیکت
-router.delete("/:id", protect, deleteTicket);
-
-// دریافت همه تیکت‌ها برای ساپورت
-router.get("/", protect, support, getAllTickets);
-
-// پاسخ به تیکت توسط ساپورت
-router.put("/:id/answer", protect, support, answerTicket);
-
-// تغییر وضعیت خوانده شدن تیکت توسط ساپورت
-router.put("/:id/read", protect, support, setTicketReadForSupport);
 
 // تغییر وضعیت خوانده شدن تیکت توسط کاربر
 router.put("/:id/read-user", protect, setTicketReadForUser);
@@ -42,7 +20,6 @@ router.put("/:id/reopen", protect, (req, res, next) => {
     }
 }, reopenTicketByAdmin);
 
-const router = express.Router();
 
 // ایجاد تیکت توسط کاربر
 router.post("/", protect, createTicket);
