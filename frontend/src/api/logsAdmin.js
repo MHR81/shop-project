eb90c966b4597cd2342ac8f0356fad401d91586d
Hosein Rahimi <hosein81rahimi@gmail.com>
2025-09-21 13:39:03 +0330

    payment33

--- frontend/src/api/logsAdmin.js
+++ frontend/src/api/logsAdmin.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 export const deleteAllLogs = async (token) => {
-  const res = await axios.delete("/api/logs-admin/all", {
+  const res = await axios.delete(`${API_URL}/logs-admin/all`, {
     headers: { Authorization: `Bearer ${token}` }
   });
   return res.data;
 };
 
 export const deleteUserLogs = async (token, userId) => {
-  const res = await axios.delete(`/api/logs-admin/user/${userId}`, {
+  const res = await axios.delete(`${API_URL}/logs-admin/user/${userId}`, {
     headers: { Authorization: `Bearer ${token}` }
   });
   return res.data;
